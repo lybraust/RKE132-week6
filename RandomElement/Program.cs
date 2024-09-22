0d3676eac64069f9b31f614466693d11249e7b15
Ly Braust <ly.braust@tktk.ee>
2024-09-22 16:13:10 +0300

    add week6 code

---
+++ RandomElement/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] snacks = { "sushi", "pizza", "burger", "chicken wings", "Asian takeaway" };
+
+Random rand = new Random();
+
+int randomIndex = rand.Next(0, snacks.Length);
+
+// Viitamine massiivi elemendile randomIndex abil
+Console.WriteLine($"Tonight we are going to eat {snacks[randomIndex]}.");
